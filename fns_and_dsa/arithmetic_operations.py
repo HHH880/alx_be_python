b1a2f2ac3a3a5df0b849932423075fa03a0f7fbf
Elvis_Kusi <135480109+HHH880@users.noreply.github.com>
2025-11-26 09:51:53 -0200

    Refactor perform_operation with type hints and error handling
    Refactor perform_operation function to include type hints and improve error handling for division and invalid operations.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,13 @@
-def perform_operation(num1, num2, operation):
-    if operation == 'add'.lower():
-         return num1 + num2
-    if operation == 'substract':
-         return num2-num1
-    if operation == 'multiply':
-         return num1 * num2
-    if operation == 'divide'.lower():
-        while num1 ==0:
-         print("Num1 should not be zero(0)")
-         return num2 / num1
-        
+def perform_operation(num1: float, num2: float, operation: str):   
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "DIVISION_BY_ZERO"
+        return num1 / num2
+    else:
+        return "INVALID_OPERATION"
