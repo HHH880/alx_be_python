c74c1774b491b236989a2bbcfbdf4f3fd142ea24
Elvis_Kusi <135480109+HHH880@users.noreply.github.com>
2025-11-26 09:57:26 -0200

    Files uploaded
    Refactor operation checks and handle division by zero.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
-    if operation == 'add'.lower():
+    if operation == 'add':
          return num1 + num2
-    if operation == 'substract':
+    elif operation == 'substract':
          return num2-num1
-    if operation == 'multiply':
+    elif operation == 'multiply':
          return num1 * num2
-    if operation == 'divide'.lower():
-        while num1 ==0:
-         print("Num1 should not be zero(0)")
+    elif operation == 'divide':
+        if num2 == 0:
+             return "DIVISION_NOT_ZERO"
          return num2 / num1
         
