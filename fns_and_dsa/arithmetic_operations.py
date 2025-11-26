8f63b975a57cff8c6876eb8421699b2079469f1d
Elvis_Kusi <135480109+HHH880@users.noreply.github.com>
2025-11-26 09:59:18 -0200

    Files uploaded

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
     if operation == 'add':
          return num1 + num2
-    elif operation == 'substract':
-         return num2-num1
+    elif operation == 'subtract':
+         return num1-num2
     elif operation == 'multiply':
          return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
              return "DIVISION_NOT_ZERO"
-         return num2 / num1
+         return num1 / num2
         
